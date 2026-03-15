84ef017776eeb8423450f2ced51c0dd795eb5ecc
agraja38 <agraja38@users.noreply.github.com>
2026-03-15 20:13:52 +0530

    Force module resolution during SBC builds

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 var (
-	Version   = "1.0.05"
+	Version   = "1.0.06"
 	Commit    = "dev"
 	BuildDate = "unknown"
 )
--- scripts/build-backend.sh
+++ scripts/build-backend.sh
@@ -5,6 +5,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$ROOT_DIR"
 
 echo "[PiNetMonitor] Building backend binaries"
-go mod download
+export GOFLAGS="${GOFLAGS:-} -mod=mod"
+go get github.com/mattn/go-sqlite3@v1.14.24
 go build -o bin/pinetmonitord ./cmd/pinetmonitord
 go build -o bin/pinetmonitor ./cmd/pinetmonitor
